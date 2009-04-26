1b3b881ed94c7b4d6e5078448b733ddeff64e333
guilherme.rotta <guilherme.rotta@3c264ff2-0c15-11de-b025-799c3e7cca98>
2009-04-26 21:52:24 +0000

    mudança do nome

---
+++ trunk/DeepTwitter/src/gui/visualizations/GUITimeline.java
@@ -0,0 +1,20 @@
+package gui.visualizations;
+
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import prefuse.demos.Congress;
+
+import twitter4j.Status;
+
+public class GUITimeline extends JFrame{
+	public GUITimeline(ArrayList<Status> statusesList) {
+		super("Timeline");
+		setContentPane(new TimelinePanel(statusesList));
+        pack();
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setVisible(true);
+	}
+}
