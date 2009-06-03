e4748350dea329e9abc77a039891e7ada82f68f6
guilherme.rotta <guilherme.rotta@3c264ff2-0c15-11de-b025-799c3e7cca98>
2009-06-03 05:20:32 +0000

    Apenas inserindo nome do usuario no timeline. Verificar o metodo s.getUser().getUtcOffset() e permitir a exibicao dos updates pelo timezone do usuario!

--- trunk/DeepTwitter/src/gui/visualizations/GUITimeline.java
+++ trunk/DeepTwitter/src/gui/visualizations/GUITimeline.java
@@ -8,9 +8,9 @@ import twitter4j.Status;
 import twitter4j.TwitterResponse;
 
 public class GUITimeline extends JFrame{
-	public GUITimeline(List<TwitterResponse> statusesList, boolean isGroup) {
+	public GUITimeline(List<TwitterResponse> statusesList, boolean isGroup, String name) {
 		super("Timeline");
-		setContentPane(new TimelinePanel(statusesList,isGroup));
+		setContentPane(new TimelinePanel(statusesList,isGroup,name));
         pack();
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setVisible(true);
